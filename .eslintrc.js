5fc3cf9049880938da4cc8cfec78f49c892f1db2
ngoc.maituan <ngoc.mai@silentium.io>
2021-05-06 20:56:16 +0700

    initialize eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 80,
+        semicolons: true,
+        jsxSingleQuote: true,
+      },
+    ],
+    'react/prop-types': 'off',
+  },
+};
