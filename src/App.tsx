c70a62fa8f6561483aa2bc094516d9f1ee9630fd
ngoc.maituan <ngoc.mai@silentium.io>
2021-05-10 17:03:32 +0700

    clean react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { WeatherForecastBrowser } from './components';
-import { QueryClientProvider, QueryClient } from 'react-query';
 
 import 'antd/dist/antd.css';
 
-const queryClient = new QueryClient();
-
 export const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className='App'>
-        <WeatherForecastBrowser />
-      </div>
-    </QueryClientProvider>
+    <div className='App'>
+      <WeatherForecastBrowser />
+    </div>
   );
 };
